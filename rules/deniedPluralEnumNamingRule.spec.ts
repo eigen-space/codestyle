4bdab67ca2729b1ef36803c52b947d0fa0f873a9
Daniil Sitdikov <hello.boriskas@gmail.com>
2019-03-20 12:59:40 +0300

    codestyle/rules: added denied plural in the names of enums rule

---
+++ rules/deniedPluralEnumNamingRule.spec.ts
@@ -0,0 +1,39 @@
+/* tslint:disable:file-name-casing comment-type */
+import { lint } from '../test/linter';
+
+const rule = 'denied-plural-enum-naming';
+
+describe('DeniedPluralEnumNamingRule', () => {
+
+    it('should return error if the name of an enum with export is plural', () => {
+        const source = `
+            export enum CardColumnsTemplateSizes
+        `;
+        const result = lint(rule, source);
+        expect(result.errorCount).toBe(1);
+    });
+
+    it('should return error if the name of an enum is plural', () => {
+        const source = `
+            enum CardColumnsTemplateSizes
+        `;
+        const result = lint(rule, source);
+        expect(result.errorCount).toBe(1);
+    });
+
+    it('should not return error if the name of an enum is singular', () => {
+        const source = `
+            enum CardColumnsTemplateSize
+        `;
+        const result = lint(rule, source);
+        expect(result.errorCount).toBe(0);
+    });
+
+    it('should not return error if the name of an enum is uncountable singular', () => {
+        const source = `
+            enum CardAxis
+        `;
+        const result = lint(rule, source);
+        expect(result.errorCount).toBe(0);
+    });
+});
