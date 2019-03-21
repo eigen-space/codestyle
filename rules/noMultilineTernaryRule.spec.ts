91d0874ad835354e6e5e1971ff1f4d7d5f16c21a
Nikita Sergeev <sergeev.nickitos@yandex.ru>
2019-03-21 14:12:25 +0300

    COREUI-231 common: added no-multiline-ternary rule

---
+++ rules/noMultilineTernaryRule.spec.ts
@@ -0,0 +1,26 @@
+/* tslint:disable:file-name-casing comment-type */
+import { lint } from '../test/linter';
+import { Rule } from './noMultilineTernaryRule';
+
+const rule = 'no-multiline-ternary';
+
+describe('noMultilineTernaryRule', () => {
+
+    it('should return error if ternary expression wrote in more than one line', () => {
+        const source = `
+            return true
+              ? 1
+              : 2;
+        `;
+        const failure = lint(rule, source).failures[0].getFailure();
+        expect(failure).toBe(Rule.FAILURE_STRING);
+    });
+
+    it('should not return error ternary expression wrote in one line', () => {
+        const source = `
+            const a = true ? 1 : 2;
+        `;
+        const result = lint(rule, source);
+        expect(result.errorCount).toBe(2);
+    });
+});
