7f1a5ac7cd420691b95beb10e365b371f47d8a19
Daniil Sitdikov <hello.boriskas@gmail.com>
2019-04-06 15:47:50 +0300

    codestyle/rules/object-properties-carrying: added scan through inner objects, updated specs; common: added custom tslint

--- test/test-sandbox.ts
+++ test/test-sandbox.ts
@@ -1,84 +1,20 @@
-const j = 10;
-const obj108 = { y: 123, u: 0, i: 9, j };
-
-const a123123 = { component: 12 };
-
-const obj18 = {
-    r: 12,
-    u: 80,
-    y: 16,
-    o: 13,
-    t: (): number => {
-        return 2;
-    },
-    io: 'qwerty'
-};
-
-const obj181 = {
-    r: 12,
-    u: 80,
-    y: 16,
-    o: 13,
-    t: (): number => {
-        return 2;
-    }, io: 'qwerty'
-};
-
-const obj185 = {
-    r: 12, t: (): number => {
-        return 2;
+const nestedObjects = {
+    someObject: 123,
+    inner: {
+        someInnerObject: {
+            test: 123,
+            another: 123
+        }
     }
 };
 
-class Main {
-    static defaultProps = {
-        onBurgerButtonClick: () => {},
-        onMenuClick: () => {}
-    };
-}
-
-const obj12 = {
-    r: 12, u: 80,
-    y: 16,
-    o: 13
-};
-
-const obj14 = {
-    r: 12, y: 16,
-    u: 80
-};
-
-const obj13 = {
-    r: 12,
-    u: 80,
-    y: 16
-};
-
-const a = 10;
-const obj102 = {
-    y: 123,
-    u: 0,
-    i: 9,
-    a
-};
-
-const obji = { yellow: () => {}, green: () => {} };
-
-const objiy = { green: function (): number { return 2; } };
-
-const layout = {
-    gridBreakPoints: {
-        sm: 576,
-        md: 768,
-        lg: 992,
-        xlg: 1200
+const colors = {
+    shades: {
+        black100: '12',
+        black40: '213'
     }
 };
 
-const obj188 = { r: 12, u: 80, y: 16, k: 1, l: 9, z: 10 };
+const someTest = { default: colors.shades.black100, primary: colors.shades.black40 };
 
-const props = {
-    columnsTemplate: ['OLololo lolo lo lolllll', 'Qwerty ololo', '123123']
-};
-
-let some: boolean;
+const props = { columnsTemplate: 'Hello! Did you hear me?', test: 'California Dreaming! Noooooo' };
