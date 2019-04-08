16a81c0f4d6786cbc94ad3f1aadc920876d0dd7e
Daniil Sitdikov <hello.boriskas@gmail.com>
2019-04-08 15:35:03 +0300

    codestyle/common: restructure project

--- rules/objectPropertiesCarryingRule.spec.ts
+++ rules/objectPropertiesCarryingRule.spec.ts
@@ -1,5 +1,5 @@
 // tslint:disable:file-name-casing
-import { lint } from '../test/linter';
+import { lint } from '../sandbox/linter';
 import { Rule } from './objectPropertiesCarryingRule';
 
 const rule = 'object-properties-carrying';
