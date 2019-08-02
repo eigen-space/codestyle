26949f70aa092719e3f7a8546f60cb0d8621c1ca
Daniil Sitdikov <hello.boriskas@gmail.com>
2019-08-02 16:15:50 +0300

    feature/redux: replaced 2 to error

--- src/packages/eslint/redux.js
+++ src/packages/eslint/redux.js
@@ -3,9 +3,9 @@ module.exports = {
         'react-redux'
     ],
     rules: {
-        'react-redux/connect-prefer-named-arguments': 2,
-        'react-redux/prefer-separate-component-file': 2,
-        'react-redux/mapStateToProps-prefer-parameters-names': 2,
-        'react-redux/mapStateToProps-no-store': 2
+        'react-redux/connect-prefer-named-arguments': 'error',
+        'react-redux/prefer-separate-component-file': 'error',
+        'react-redux/mapStateToProps-prefer-parameters-names': 'error',
+        'react-redux/mapStateToProps-no-store': 'error'
     }
 };
