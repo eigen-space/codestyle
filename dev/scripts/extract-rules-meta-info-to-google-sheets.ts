a9b3b8c54573f1ed1314031e679897243e46e1cb
Nikita Sergeev <nikita.sergeev@arrival.services>
2019-09-06 15:31:40 +0300

    COREUI-518 scripts/extract-rules-meta-info-to-google-sheets: rewrote script on typescript

--- dev/scripts/extract-rules-meta-info-to-google-sheets.ts
+++ dev/scripts/extract-rules-meta-info-to-google-sheets.ts
@@ -1,10 +1,12 @@
-const path = require('path');
-const fs = require('fs');
-const os = require('os');
-const googleSpreadsheet = require('google-spreadsheet');
-const creds = require('../credentials/google-sheets-credentials.json');
-const { promisify } = require('util');
-const { walkThrough } = require('@eigenspace/helper-scripts');
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+import { Cell, Info, Worksheet } from 'google-spreadsheet';
+import * as creds from '../credentials/google-sheets-credentials.json';
+import { promisify } from 'util';
+import { walkThrough } from '@eigenspace/helper-scripts';
+
+const GoogleSpreadsheet = require('google-spreadsheet');
 
 // The Google Sheet ID found in the URL of your Google Sheet.
 const SPREADSHEET_ID = '1phevI9VxclD8QsHYr5Stu-nlvUjJ3taBujgu1JOUqlg';
@@ -12,15 +14,23 @@ const SPREADSHEET_ID = '1phevI9VxclD8QsHYr5Stu-nlvUjJ3taBujgu1JOUqlg';
 const PATH_TO_DOCS = './doc';
 const FILE_NAME = 'README.ru.md';
 
-const StatusType = {
-    AUTOMATED: 'Автоматизировано',
-    NON_AUTOMATED: 'Не автоматизировано',
-    PARTLY_AUTOMATED: 'Частично автоматизировано'
-};
+interface Rule {
+    id: string;
+    name: string;
+    status: string;
+    rule: string;
+    localized: string;
+    violations: string;
+    link: string;
+
+    [key: string]: string | number;
+}
+
+const StatusTypes = ['Автоматизировано', 'Не автоматизировано', 'Частично автоматизировано'];
 
 const ID_PATTERN = /(([#]+ ([\d+.]+))|(\w\.))/.toString()
     .slice(1, -1);
-const STATUS_PATTERN = Object.values(StatusType).map(statusType => `(${statusType})`)
+const STATUS_PATTERN = StatusTypes.map(statusType => `(${statusType})`)
     .join('|');
 const END_PATTERN = new RegExp(`(${os.EOL}.+)*`).toString()
     .slice(1, -1);
@@ -31,17 +41,17 @@ const COLUMNS_COUNT = 7;
 
 accessSpreadsheet();
 
-async function accessSpreadsheet() {
+async function accessSpreadsheet(): Promise<void> {
     // Create a document object using the ID of the spreadsheet - obtained from its URL.
-    const doc = new googleSpreadsheet(SPREADSHEET_ID);
+    const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
     // Authenticate with the Google Spreadsheets API.
     await promisify(doc.useServiceAccountAuth.bind(doc))(creds);
     // Get info about worksheets
-    const info = await promisify(doc.getInfo)();
+    const info = await promisify(doc.getInfo)() as Info;
 
-    const sheets = info.worksheets;
+    const sheets: Worksheet[] = info.worksheets;
 
-    walkThrough(PATH_TO_DOCS, async (dir, file) => {
+    walkThrough(PATH_TO_DOCS, async (dir: string, file: string) => {
         if (file !== FILE_NAME) {
             return;
         }
@@ -49,6 +59,10 @@ async function accessSpreadsheet() {
         const docType = dir.split(path.sep)
             .pop();
         const sheet = sheets.find(worksheet => worksheet.title === docType);
+
+        if (!sheet) {
+            return;
+        }
 
         await sheet.clear(() => {
         });
@@ -58,23 +72,24 @@ async function accessSpreadsheet() {
     });
 }
 
-async function getCellsTillRow(sheet, maxRow) {
+async function getCellsTillRow(sheet: Worksheet, maxRow: number): Promise<Cell[]> {
+    // @ts-ignore
     return promisify(sheet.getCells)({
         'min-row': 2,
         'max-row': maxRow + 1,
         'max-col': COLUMNS_COUNT,
         'min-col': 1,
         'return-empty': true
-    });
+    }) as Promise<Cell[]>;
 }
 
-async function addRows(sheet, rules) {
+async function addRows(sheet: Worksheet, rules: Rule[]): Promise<void> {
     const cells = await getCellsTillRow(sheet, rules.length);
     const subRuleIdRegExp = /^[a-z]/;
 
     let currentParentId = '';
 
-    rules.forEach((rule, i) => {
+    rules.forEach((rule: Rule, i: number) => {
         let { id } = rule;
 
         if (!subRuleIdRegExp.test(id)) {
@@ -94,7 +109,7 @@ async function addRows(sheet, rules) {
     return sheet.bulkUpdateCells(cells);
 }
 
-function getRulesData(pathToDoc) {
+function getRulesData(pathToDoc: string): Rule[] {
     const doc = fs.readFileSync(pathToDoc, 'utf8');
     const rules = doc.match(RULE_REGEXP);
 
@@ -104,14 +119,12 @@ function getRulesData(pathToDoc) {
 
     return rules.map(rule => {
         RULE_REGEXP.lastIndex = 0;
-        const execResult = RULE_REGEXP.exec(rule);
 
-        // Each index belongs to data in parsed result
-        const id = execResult[3] || execResult[4];
-        const status = execResult[5];
-        const ruleName = execResult[9].slice(1)
+        const [, , , ruleId, subRuleId, status, , , , rawRuleName, name] = RULE_REGEXP.exec(rule) as Array<string>;
+
+        const id = ruleId || subRuleId;
+        const ruleName = rawRuleName.slice(1)
             .trim();
-        const name = execResult[10];
 
         return {
             id,
