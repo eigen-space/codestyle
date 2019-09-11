e6c2a199e0cc86a8bbc53393c975baa4e54d08da
Nikita Sergeev <nikita.sergeev@arrival.services>
2019-09-11 22:10:01 +0300

    #70 common: changed structure of plugins

---
+++ src/packages/redux-rules/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+    rules: {},
+    configs: {
+        all: {
+            plugins: [
+                'react-redux'
+            ],
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                }
+            },
+            rules: {
+                'react-redux/connect-prefer-named-arguments': 'error',
+                'react-redux/prefer-separate-component-file': 'error',
+                'react-redux/mapStateToProps-prefer-parameters-names': 'error',
+                'react-redux/mapStateToProps-no-store': 'error'
+            }
+        }
+    }
+};
