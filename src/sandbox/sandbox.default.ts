39b1eb7e70871e0e305c31668da7edbce6b8baa4
Nikita Sergeev <nikita.sergeev@arrival.services>
2019-09-15 00:39:42 +0300

    #68 rules: fixed specs and coverage

--- src/sandbox/sandbox.default.ts
+++ src/sandbox/sandbox.default.ts
@@ -1,24 +1,3 @@
-const a = 1;
-const b = '1';
-
-// Плохо
-a == b; // Вернёт true
-
-// Хорошо
-a === b; // Вернёт false
-
-const x = 15;
-const arr = [15];
-
-x > 1;
-x > 1 && x < 10;
-arr.length > 5;
-
-// Хорошо
-1 < x;
-1 < x && x < 10;
-5 < arr.length;
-
 enum BusinessCommon {
     ES_EXEC
 }
@@ -30,48 +9,3 @@ if (BusinessCommon.ES_EXEC !== trip.state) {
 
 if (trip.state !== BusinessCommon.ES_EXEC) {
 }
-
-function someAction() {
-}
-
-// Плохо
-if (trip.disabled) {
-    if (trip.hasOrders) {
-        someAction();
-    }
-}
-
-// Хорошо
-if (trip.disabled && trip.hasOrders) {
-    someAction();
-}
-
-// Плохо
-if (!(a && b)) {
-    if ((a < b) && (x < a)) {
-    }
-}
-
-// Хорошо
-if (!a || !b) {
-    if (a < b && x < a) {
-    }
-}
-
-// Плохо
-if (a) {
-    const result = someAction();
-} else {
-    const result2 = 'someValue';
-}
-
-// Хорошо
-if (a) {
-    const result = someAction();
-}
-
-// Плохо
-trip.attributes = trip.attributes ? trip.attributes : [];
-
-// Хорошо
-trip.attributes = trip.attributes || [];
