5c5710824cb2e1dfa0f5674b1070ba7810be3056
Nikita Agupov <nktrsk@gmail.com>
2019-09-23 19:13:23 +0300

    #54 common: converted rule files to typescript

--- src/packages/eslint-plugin-eigenspace-script/lib/rules/conditions.ts
+++ src/packages/eslint-plugin-eigenspace-script/lib/rules/conditions.ts
@@ -1,6 +1,8 @@
-const { isParenthesized } = require('eslint-utils');
+import { isParenthesized } from 'eslint-utils';
+import { CommonRuleContext } from '../../@types/eslint';
+import { AST_NODE_TYPES, TSESTree } from '@typescript-eslint/experimental-utils';
 
-const ERROR_TYPE = {
+export const ERROR_TYPE = {
     COUNTER_CLOCKWISE_COMPARISON: 'counter-clockwise-comparison',
     LITERAL_OR_CONSTANT_COMPARISON: 'literal-or-constant-comparison',
     NEGATION_BEFORE_PARENTHESES: 'negation-before-parentheses',
@@ -19,7 +21,7 @@ const MESSAGE_TYPE = {
     [ERROR_TYPE.UNNECESSARY_TERNARY]: 'Unnecessary ternary operator'
 };
 
-const exportedModule = module.exports = {
+export default {
     meta: {
         type: 'layout',
 
@@ -37,63 +39,61 @@ const exportedModule = module.exports = {
             [ERROR_TYPE.UNNECESSARY_TERNARY]: MESSAGE_TYPE[ERROR_TYPE.UNNECESSARY_TERNARY]
         }
     },
-    create(context) {
+    create(context: CommonRuleContext) {
         return {
-            BinaryExpression(node) {
+            BinaryExpression(node: TSESTree.BinaryExpression) {
                 checkBinaryExpression(context, node);
             },
 
-            LogicalExpression(node) {
+            LogicalExpression(node: TSESTree.LogicalExpression) {
                 checkLogicalExpression(context, node);
             },
 
-            AssignmentExpression(node) {
+            AssignmentExpression(node: TSESTree.AssignmentExpression) {
                 checkAssignmentExpression(context, node);
             },
 
-            VariableDeclarator(node) {
+            VariableDeclarator(node: TSESTree.VariableDeclarator) {
                 checkVariableDeclarator(context, node);
             },
 
-            IfStatement(node) {
+            IfStatement(node: TSESTree.IfStatement) {
                 checkIfStatement(context, node);
             }
         };
     }
 };
 
-exportedModule.ERROR_TYPE = ERROR_TYPE;
-
-function checkBinaryExpression(context, node) {
+function checkBinaryExpression(context: CommonRuleContext, node: TSESTree.BinaryExpression): void {
     isCounterClockwiseComparison(context, node);
     isComparisonHasEnumOrConstantOnLeftSide(context, node);
     isNegationBeforeParentheses(context, node);
 }
 
-function checkLogicalExpression(context, node) {
+function checkLogicalExpression(context: CommonRuleContext, node: TSESTree.LogicalExpression): void {
     isNegationBeforeParentheses(context, node);
 }
 
-function checkAssignmentExpression(context, node) {
+function checkAssignmentExpression(context: CommonRuleContext, node: TSESTree.AssignmentExpression): void {
     isHasUnnecessaryTernary(context, node.right);
 }
 
-function checkVariableDeclarator(context, node) {
-    isHasUnnecessaryTernary(context, node.init || {});
+function checkVariableDeclarator(context: CommonRuleContext, node: TSESTree.VariableDeclarator): void {
+    isHasUnnecessaryTernary(context, node.init);
 }
 
-function checkIfStatement(context, node) {
+function checkIfStatement(context: CommonRuleContext, node: TSESTree.IfStatement): void {
     isHasUnnecessaryIfStatement(context, node);
     isHasUnnecessaryElseStatement(context, node);
 }
 
-function isCounterClockwiseComparison(context, node) {
+function isCounterClockwiseComparison(context: CommonRuleContext, node: TSESTree.BinaryExpression): void {
     if (node.operator.startsWith('>')) {
         report(context, node, ERROR_TYPE.COUNTER_CLOCKWISE_COMPARISON);
     }
 }
 
-function isComparisonHasEnumOrConstantOnLeftSide(context, node) {
+function isComparisonHasEnumOrConstantOnLeftSide(context: CommonRuleContext, node: TSESTree.BinaryExpression): void {
     if (['!==', '==='].includes(node.operator)) {
         const isLeftSideVariableConstOrLiteral = isPossibleEnum(context, node.left) || isLiteral(node.left);
         const isRightSideVariableConstOrLiteral = isPossibleEnum(context, node.right) || isLiteral(node.right);
@@ -104,8 +104,8 @@ function isComparisonHasEnumOrConstantOnLeftSide(context, node) {
     }
 }
 
-function isHasUnnecessaryTernary(context, node) {
-    if (node.type !== 'ConditionalExpression') {
+function isHasUnnecessaryTernary(context: CommonRuleContext, node: TSESTree.Node | null): void {
+    if (!node || node.type !== AST_NODE_TYPES.ConditionalExpression) {
         return;
     }
 
@@ -119,9 +119,11 @@ function isHasUnnecessaryTernary(context, node) {
     }
 }
 
-function isHasUnnecessaryIfStatement(context, node) {
+function isHasUnnecessaryIfStatement(context: CommonRuleContext, node: TSESTree.IfStatement): void {
+    // TODO Case when prop body doesn't exists
+    // @ts-ignore
     const body = [...node.consequent.body];
-    if (!body.length || body.pop().type !== 'IfStatement') {
+    if (!body.length || body.pop().type !== AST_NODE_TYPES.IfStatement) {
         return;
     }
 
@@ -130,72 +132,94 @@ function isHasUnnecessaryIfStatement(context, node) {
     }
 }
 
-function isHasUnnecessaryElseStatement(context, node) {
+function isHasUnnecessaryElseStatement(context: CommonRuleContext, node: TSESTree.IfStatement): void {
+    // TODO Case when prop body doesn't exists
+    // @ts-ignore
     if (!node.alternate || !node.alternate.body) {
         return;
     }
 
-    const { body } = node.alternate;
+    // TODO Case when prop body doesn't exists
+    // @ts-ignore
+    const body = node.alternate.body as TSESTree.Statement[];
 
     if (body.every(statement => isSimpleVariableDeclarationOrAssignment(statement))) {
         report(context, node.alternate, ERROR_TYPE.UNNECESSARY_ELSE);
     }
 }
 
-function isNegationBeforeParentheses(context, node) {
+function isNegationBeforeParentheses(context: CommonRuleContext, node: TSESTree.Node): void {
     const sourceCode = context.getSourceCode();
     if (isParenthesized(1, node, sourceCode)) {
-        const tokenBeforeParentheses = sourceCode.getTokenBefore(sourceCode.getTokenBefore(node));
+        const tokenBeforeParentheses = sourceCode.getTokenBefore(sourceCode.getTokenBefore(node)!);
 
-        if (tokenBeforeParentheses.value === '!' && tokenBeforeParentheses.type === 'Punctuator') {
+        if (tokenBeforeParentheses!.value === '!' && tokenBeforeParentheses!.type === 'Punctuator') {
             report(context, node, ERROR_TYPE.NEGATION_BEFORE_PARENTHESES);
         }
     }
 }
 
-function isSimpleVariableDeclarationOrAssignment(node) {
-    const isAssignment = node.type === 'ExpressionStatement' && node.expression.type === 'AssignmentExpression';
-    if (node.type !== 'VariableDeclaration' && !isAssignment) {
+function isSimpleVariableDeclarationOrAssignment(node: TSESTree.Statement): boolean {
+    if (node.type !== AST_NODE_TYPES.VariableDeclaration && !isAssignment(node)) {
         return false;
     }
 
-    if (isAssignment) {
+    if (isAssignment(node)) {
+        // TODO Case when prop doesn't exists
+        // @ts-ignore
         return isSimpleInitialization(node.expression.right);
     }
 
+    // TODO Case when prop doesn't exists
+    // @ts-ignore
     return node.declarations.every(declaration => isSimpleInitialization(declaration.init));
 }
 
-function isSimpleInitialization(node) {
+function isSimpleInitialization(node: TSESTree.Node): boolean {
     return isSimpleProperty(node) || isSimpleLogicalOrBinaryExpression(node);
 }
 
-function isSimpleLogicalOrBinaryExpression(node) {
-    const isLogicalOrBinaryExpression = ['LogicalExpression', 'BinaryExpression'].includes(node.type);
+function isSimpleLogicalOrBinaryExpression(node: TSESTree.Node): boolean {
+    const types = [AST_NODE_TYPES.LogicalExpression, AST_NODE_TYPES.BinaryExpression];
+    const isLogicalOrBinaryExpression = types.includes(node.type);
+    // TODO Typescript sends fake errors
+    // @ts-ignore
     return isLogicalOrBinaryExpression && isSimpleProperty(node.left) && isSimpleProperty(node.right);
 }
 
-function isSimpleProperty(node) {
-    return ['Literal', 'Identifier', 'MemberExpression'].includes(node.type);
+function isSimpleProperty(node: TSESTree.Node): boolean {
+    const types = [
+        AST_NODE_TYPES.Literal,
+        AST_NODE_TYPES.Identifier,
+        AST_NODE_TYPES.MemberExpression
+    ];
+
+    return types.includes(node.type);
 }
 
-function isPossibleEnum(context, node) {
+function isPossibleEnum(context: CommonRuleContext, node: TSESTree.Node): boolean {
     // If first letter of object is in upper case then perhaps it is an enum or constant
 
     const sourceCode = context.getSourceCode();
 
     const variableText = sourceCode.getText(node);
 
-    return node.type === 'MemberExpression' && variableText[0] !== variableText[0].toLowerCase();
+    return node.type === AST_NODE_TYPES.MemberExpression && variableText[0] !== variableText[0].toLowerCase();
 }
 
-function isLiteral(node) {
-    return ['Literal', 'Identifier'].includes(node.type);
+function isLiteral(node: TSESTree.Node): boolean {
+    return [AST_NODE_TYPES.Literal, AST_NODE_TYPES.Identifier].includes(node.type);
 }
 
-function report(context, node, messageId) {
-    context.report({
-        messageId,
-        loc: node.loc
-    });
+function isAssignment(node: TSESTree.Node): boolean {
+    if (node.type !== AST_NODE_TYPES.ExpressionStatement) {
+        return false;
+    }
+
+    return node.expression.type === AST_NODE_TYPES.AssignmentExpression;
+
+}
+
+function report(context: CommonRuleContext, node: TSESTree.Node, messageId: string): void {
+    context.report({ messageId, loc: node.loc });
 }
