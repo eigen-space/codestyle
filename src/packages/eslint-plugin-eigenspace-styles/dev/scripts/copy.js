bfc09b8b657c167f010d1f60d94849b2d793c8b4
Danilqa <hello.boriskas@gmail.com>
2019-10-16 21:06:06 +0300

    #96 eslint-plugin-eigenspace-styles common: removed typescript

--- src/packages/eslint-plugin-eigenspace-styles/dev/scripts/copy.js
+++ src/packages/eslint-plugin-eigenspace-styles/dev/scripts/copy.js
@@ -2,7 +2,7 @@ const { removeDirectory, copy } = require('@eigenspace/helper-scripts');
 const fs = require('fs');
 
 const outputDir = './dist';
-const files = ['package.json', 'yarn.lock', 'README.md'];
+const files = ['package.json', 'yarn.lock', 'README.md', 'src/index.js'];
 
 if (fs.existsSync(outputDir)) {
     removeDirectory(outputDir);
