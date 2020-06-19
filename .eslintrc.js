73c7c671669880627763a29aa30b9e5013735d50
Danilqa <hello.boriskas@gmail.com>
2020-06-19 16:42:36 +0300

    common: moved performance package to the react scope

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
 module.exports = {
-    plugins: ['react-perf'],
     extends: [
-        'plugin:react-perf/all',
         'plugin:eigenspace-script/all',
         'plugin:eigenspace-react/all',
         'plugin:eigenspace-redux/all',
